66f117649b6528d50930b428a66f05c27d090bae
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-03-24 12:39:29 +0100

    [DEVEX-185] Removed leftover folders that had KurrentDb instead of KurrentDB
    It seems that after conflicts when merging StreamRevision PR, the code before merging stayed as duplicated without capital B in KurrentDb.
    
    I checked by doing copying with override from those folders to the proper one, and there were no differences besides those stream revision changes, so it's safe to just remove them.

--- test/KurrentDb.Client.Tests.Common/FluentDocker/TestContainerService.cs
+++
@@ -1,6 +0,0 @@
-using Ductus.FluentDocker.Builders;
-using Ductus.FluentDocker.Services;
-
-namespace KurrentDB.Client.Tests.FluentDocker;
-
-public abstract class TestContainerService : TestService<IContainerService, ContainerBuilder>;
